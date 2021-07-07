497b60aca1f2494c15565f68962c223ec13a8218
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-07-07 19:12:36 +0300

    odify Notes

--- dbo/Stored Procedures/p_note_update.sql
+++ dbo/Stored Procedures/p_note_update.sql
@@ -2,9 +2,9 @@
 CREATE PROCEDURE p_note_update
 	@customer_id int,
 	@note_id int,
-	@note nvarchar(500)
+	@line nvarchar(500)
 AS
 UPDATE [dbo].[Notes]
-SET	[dbo].[Notes].[Note] = @note
+SET	[dbo].[Notes].[Line] = @line
 WHERE	[dbo].[Notes].[CustomerID] = @customer_id AND
 		[dbo].[Notes].[NoteID] = @note_id;
--- dbo/Tables/Notes.sql
+++ dbo/Tables/Notes.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Notes] (
     [NoteID]     INT            IDENTITY (1, 1) NOT NULL,
     [CustomerID] INT            NOT NULL,
-    [Note]       NVARCHAR (500) NOT NULL,
+    [Line]       NVARCHAR (500) NOT NULL,
     CONSTRAINT [PK_Note] PRIMARY KEY CLUSTERED ([NoteID] ASC),
     CONSTRAINT [FK_Notes_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID]) ON DELETE CASCADE ON UPDATE CASCADE
 );
